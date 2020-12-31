abd9951c962c48e0b7865f2de1048b2713a8364d
Daniel Almasi <almd@pm.me>
2020-12-31 22:00:45 +0000

    Update email href script to comply with CSPv3
    Update email link to comply with CSP3
    
    Update email script
    
    Update email script

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,1 +1,14 @@
-const getContactEmail=()=>{window.location.href=`mailto:${atob("ZW5xdWlyaWVzQGFsbWFzaS5kZXY=")}`};
+/**
+ * Decode email address into mailto href link and click it.
+ * (for spam protection purposes)
+ */
+const getContactEmail = () => {
+    window.location.href = `mailto:${atob("ZW5xdWlyaWVzQGFsbWFzaS5kZXY=")}`;
+};
+/**
+ * Add event handler for emailLink element.
+ * CSP v3 compliant
+ */
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('emailLink').addEventListener('click', getContactEmail);
+});
